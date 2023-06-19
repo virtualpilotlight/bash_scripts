c28089a2edfad7ebb4156fc9bf6d744abc0137ff
Omega X Rae <63996271+virtualpilotlight@users.noreply.github.com>
2023-06-19 19:07:26 -0400

    Update scan_test.sh

--- scan_test.sh
+++ scan_test.sh
@@ -6,7 +6,7 @@ echo "You don't have an IP address specified"
 echo "syntax ./nmap_test.sh 10.10.2.20"
 
 else
-ping -c 1 10.10.10.161 >> results.txt
+ping -c 1 $1 >> results.txt
 ports=$(nmap -p- --min-rate=1000 -T4 $1 | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
 nmap -sC -sV -p$ports $1 >> results.txt
 fi
